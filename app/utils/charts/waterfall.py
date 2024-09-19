fcffae5d2aca137a5cdd0b55a65b6ceac8065f25
Gabriella Braz <gabibraz15@outlook.com>
2024-09-18 22:44:13 -0300

    5

--- app/utils/charts/waterfall.py
+++ app/utils/charts/waterfall.py
@@ -7,14 +7,10 @@ def create_waterfall_chart(dates, auxiliary_data, income_data, expense_data):
         "title": {
             "text": "Waterfall Chart",
             "subtext": "From ExcelHome",
-            "sublink": "http://e.weibo.com/1341556070/Aj1J2x5a5",
         },
         "tooltip": {
             "trigger": "axis",
             "axisPointer": {"type": "shadow"},
-            "formatter": JsCode(
-                "function(params){var tar;if(params[1].value!=='-'){tar=params[1]}else{tar=params[0]}return tar.name+'<br/>'+tar.seriesName+' : '+tar.value}"
-            ).js_code,
         },
         "legend": {"data": ["Expense", "Income"]},
         "grid": {"left": "3%", "right": "4%", "bottom": "3%", "containLabel": True},
@@ -60,10 +56,44 @@ def create_waterfall_chart(dates, auxiliary_data, income_data, expense_data):
     st_echarts(options=options, height="500px")
 
 
+import pandas as pd
+
+
+def prepare_waterfall_data(df, initial_value):
+    auxiliary_data = [0] * (len(df) + 2)  # Incluímos 2 barras antes do df
+    income_data = ["-"] * (len(df) + 2)  # As receitas começam vazias
+    expense_data = ["-"] * (len(df) + 2)  # As despesas também começam vazias
+
+    income_data[0] = initial_value
+
+    if "agencia" in df.columns:
+        income_data[1] = int(df["agencia"].sum())
+
+    cumulative_sum = income_data[1]
+
+    for i in range(2, len(df) + 2):
+        expense_value = -int(df.iloc[i - 2]["agencia"])
+        auxiliary_data[i] = cumulative_sum
+        cumulative_sum -= expense_value
+        expense_data[i] = expense_value if expense_value != 0 else "-"
+
+    dates = ["Start", "Agency Total"] + df["status"].tolist()
+    return dates, auxiliary_data, income_data, expense_data
+
+
+data = {
+    "status": ["Status 1", "Status 2", "Status 3", "Status 4"],  # Nomes das barras
+    "agencia": [400, 300, 500, 200],  # Contagem de agências para cada status
+}
+
+df = pd.DataFrame(data)
+
+dates, auxiliary_data, income_data, expense_data = prepare_waterfall_data(df, 900)
+
 # Example usage
-dates = [f"November {i}" for i in range(1, 12)]
-auxiliary_data = [0, 900, 1245, 1530, 1376, 1376, 1511, 1689, 1856, 1495, 1292]
-income_data = [900, 345, 393, "-", "-", 135, 178, 286, "-", "-", "-"]
-expense_data = ["-", "-", "-", 108, 154, "-", "-", "-", 119, 361, 203]
+# dates = [f"November {i}" for i in range(1, 12)]
+# auxiliary_data = [0, 900, 1245, 1530, 1376, 1376, 1511, 1689, 1856, 1495, 1292]
+# income_data = [900, 345, 393, "-", "-", 135, 178, 286, "-", "-", "-"]
+# expense_data = ["-", "-", "-", 108, 154, "-", "-", "-", 119, 361, 203]
 
 create_waterfall_chart(dates, auxiliary_data, income_data, expense_data)

4a64a3496ba63e682c0287775883cfcc117aa0de
Gabriella Braz <gabibraz15@outlook.com>
2024-12-04 16:12:13 -0300

    [ADD] TIME SERIES

--- app/utils/charts/time_serie_echart.py
+++ app/utils/charts/time_serie_echart.py
@@ -1,67 +1,9 @@
-from math import ceil
 from datetime import datetime
 
-import numpy as np
 import pandas as pd
 import streamlit_echarts as st_echarts
 
-from typing import List
-from dataclasses import dataclass
-
-
-@dataclass
-class Farol:
-    nome_etl: str
-    nome_front: str
-    cor: str
-    cor_fonte: str
-    icones: str
-    cor_icone: str
-    valor_min: float
-    valor_max: float
-
-
-@dataclass
-class ScoreFarolMap:
-    farol_list: List[Farol]
-
-
 df = pd.read_excel("app/data/time_series_farol.xlsx")
-
-farol_1 = Farol(
-    nome_etl="farol_1",
-    nome_front="Farol 1",
-    cor="#FF5733",
-    cor_fonte="#000000",
-    icones="icon1",
-    cor_icone="#FF5733",
-    valor_min=0,
-    valor_max=150,
-)
-
-farol_2 = Farol(
-    nome_etl="farol_2",
-    nome_front="Farol 2",
-    cor="#33FF57",
-    cor_fonte="#000000",
-    icones="icon2",
-    cor_icone="#33FF57",
-    valor_min=151,
-    valor_max=250,
-)
-
-farol_3 = Farol(
-    nome_etl="farol_3",
-    nome_front="Farol 3",
-    cor="#3357FF",
-    cor_fonte="#000000",
-    icones="icon3",
-    cor_icone="#3357FF",
-    valor_min=251,
-    valor_max=350,
-)
-
-score_farol_map = ScoreFarolMap(farol_list=[farol_1, farol_2, farol_3])
 
 
 def criar_grafico_echarts(
@@ -69,7 +11,7 @@ def criar_grafico_echarts(
     data_column: str,
     line_name_column: str,
     value_column: str,
-    score_farol_map: ScoreFarolMap,
+    score_farol_map: dict,
 ):
     # CONVERTE A COLUNA PARA DATETIME
     df[data_column] = pd.to_datetime(df[data_column])
@@ -85,25 +27,24 @@ def criar_grafico_echarts(
     series = []
 
     # ITERA SOBRE OS FARÓIS DO SCOREFAROLMAP DATACLASS.
-    for farol in score_farol_map.farol_list:
+    for farol, color in score_farol_map.items():
         # FILTRA OS DADOS DO DATAFRAME PARA OBTER OS VALORES ASSOCIADOS AO FAROL ATUAL.
-        serie_data = df[df[line_name_column] == farol.nome_etl][value_column].tolist()
+        serie_data = df[df[line_name_column] == farol][value_column].tolist()
 
         # CRIA NOVA SÉRIE DO GRÁFICO
         series.append(
             {
-                "name": farol.nome_front,  # DEFINE O NOME DA SÉRIE PARA EXIBIÇÃO NA LEGENDA
+                "name": farol,  # DEFINE O NOME DA SÉRIE PARA EXIBIÇÃO NA LEGENDA
                 "type": "line",  # ESPECIFICA QUE A SÉRIE SERÁ UMA LINHA
                 "data": serie_data,  # ATRIBUI OS DADOS DA SÉRIE (VALORES DO FAROL)
-                "itemStyle": {"color": farol.cor},  # DEFINE A COR DA LINHA
+                "itemStyle": {"color": color},  # DEFINE A COR DA LINHA
                 "lineStyle": {
                     "width": 2,
-                    "color": farol.cor,
+                    "color": color,
                 },  # FORMATA A LARGURA E A COR DA LINHA
                 "label": {
-                    # "show": True,
                     "position": "top",
-                    "color": farol.cor_fonte,
+                    "color": "black",
                 },  # ADICIONA LABEL
                 "smooth": False,  # DEFINE SE A LINHA DEVE SER CURVAS OU LINEAR.
             }
@@ -113,7 +54,7 @@ def criar_grafico_echarts(
     option = {
         "tooltip": {"trigger": "axis"},  # CONFIGURA O TOOLTIP --> MOUSE SOBRE O EIXO
         "legend": {
-            "data": [farol.nome_front for farol in score_farol_map.farol_list]
+            "data": [farol_name for farol_name in score_farol_map.keys()]
         },  # DEFINE OS NOMES DAS SÉRIES NA LEGENDA
         "grid": {
             "left": "3%",
@@ -121,22 +62,15 @@ def criar_grafico_echarts(
             "bottom": "3%",
             "containLabel": True,
         },  # AJUSTA AS MARGENS DO GRÁFICO
-        "toolbox": {"feature": {"saveAsImage": {}}},
         "xAxis": {
             "type": "category",  # TIPO DO EIXO X == CATEGÓRICO (DATAS)
             "data": x_axis_data,  # VALORES DO EIXO X
+            "boundaryGap": True,
+            "axisTick": {"alignWithLabel": True},
+            "axisLabel": {"rotate": 30, "padding": [8, 4, 4, 4]},
         },
         "yAxis": {"type": "value"},  # TIPO DO EIXO Y == VALORES NUMÉRICO
         "series": series,  # ADICIONA AS SÉRIES AO GRÁFICO
-        "dataZoom": [
-            {
-                "start": 100
-                * (
-                    (len(x_axis_data) - 12) / len(x_axis_data)
-                ),  # ADICIONA A BARRA DE ZOOM NA PARTE INFERIOR DO GRÁFICO.
-                "end": 100,  # CALCULA O FIM DO ZOOM PARA A BARRA.
-            },
-        ],
     }
 
     st_echarts.st_echarts(options=option)
@@ -147,5 +81,5 @@ criar_grafico_echarts(
     data_column="Data",
     line_name_column="Farol",
     value_column="Valor",
-    score_farol_map=score_farol_map,
+    score_farol_map={"farol_1": "red", "farol_2": "green", "farol_3": "yellow"},
 )
